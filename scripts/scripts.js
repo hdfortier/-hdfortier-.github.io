3fc94006d8e0c848fe1968e38fd86c1c11fae527
hdfortier <hdfortier@amarillocollege.com>
2026-04-12 14:30:19 -0500

    Add files via upload

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -28,6 +28,34 @@ function calculateTotal() {
     totalBox.innerHTML = "Estimated Total: $" + total.toFixed(2);
 }
 
+function showReviews() {
+    let reviewOutput = document.getElementById("reviewOutput");
+
+    let guestNames = ["Crista", "Teena", "Carolyn", "Betty"]
+    let roomNames = ["Rose Room", "Merlot Room", "Rose Room", "Merlot Room"]
+    let ratings = ["5", "4", "5", "5"]
+    let comments = [
+        "We had a very relaxing stay at the Rustic Pour.",
+        "I can not wait to come back in the summer for the Sangria Social!",
+        "The Rose room was perfect for a refreshing reset!",
+        "Everything felt so charming and thoughtful. We will definitely be back!"
+    ];
+    
+    let output = "";
+
+    for (let i = 0; i< guestNames.length; i++) {
+        output += "<div class = 'review-card'>";
+        output += "<h3>" + guestNames[i] + "</h3>";
+        output += "<p><strong>Room: </strong>" + roomNames[i] + "</p>";
+        output += "<p><strong>Rating: </strong>" +ratings[i] + " / 5</p>";
+        output += "<p>" + comments[i] + "</p>";
+        output += "</div>";
+    }
+
+    reviewOutput.innerHTML = output;
+}
+
 window.onload = function () {
     calculateTotal();
+    showReviews();
 };
